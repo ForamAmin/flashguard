6434fe6911c0ee9a3d0399d4b9f6bd28a152da38
Foram <foram1002@gmail.com>
2026-09-15 22:51:47 +0530

    chore: initialize FlashGuard infrastructure

---
+++ src/main/java/com/flashguard/FlashguardApplication.java
@@ -0,0 +1,13 @@
+package com.flashguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashguard/FlashguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
